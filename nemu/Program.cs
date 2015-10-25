54deeb7e58cf748758740cbcbd3002943674b736
Anime4000 <ilham92_sakura@yahoo.com>
2015-10-25 11:38:11 +0800

    add stuff

--- nemu/Program.cs
+++ nemu/Program.cs
@@ -33,12 +33,22 @@ namespace nemu
 
 								if (args[i].Contains("master"))
 								{
-
+									IsMaster = true;
 								}
 
 								if (args[i].Contains("slave"))
 								{
+									IsMaster = false;
+								}
 
+								if (args[i].Contains("config"))
+								{
+									Config = args[++i];
+								}
+
+								if (args[i].Contains("dir"))
+								{
+									Folder = args[++i];
 								}
 							}
 
@@ -57,12 +67,22 @@ namespace nemu
 
 								if (args[i][j] == 'm')
 								{
-
+									IsMaster = true;
 								}
 
 								if (args[i][j] == 's')
 								{
+									IsMaster = false;
+								}
 
+								if (args[i][j] == 'c')
+								{
+									Config = args[++i];
+								}
+
+								if (args[i][j] == 'd')
+								{
+									Folder = args[++i];
 								}
 							}
 						}
@@ -82,7 +102,7 @@ namespace nemu
 		static void Help()
 		{
 			Console.WriteLine();
-			Console.WriteLine("Usage: nemu [OPTION...] CONFIG.INI FOLDER");
+			Console.WriteLine("Usage: nemu [OPTION...] INPUT");
 			Console.WriteLine();
 			Console.WriteLine("Mandatory arguments to long options are mandatory for short options too.");
 			Console.WriteLine();
@@ -91,12 +111,13 @@ namespace nemu
 			Console.WriteLine("  -v                           show version and exit");
 			Console.WriteLine("  -m, --master                 start NEMU as master server");
 			Console.WriteLine("  -s, --slave                  start NEMU as slave server (impiles -m)");
-			Console.WriteLine("  CONFIG.INI                   (optional) override default path config.ini");
-			Console.WriteLine("  FOLDER                       (optional) override default from config.ini");
+			Console.WriteLine("  -c, --config CONFIG.INI      (optional) override default path config.ini");
+			Console.WriteLine("  -d, --dir FOLDER             (optional) override default from config.ini");
+			Console.WriteLine("  INPUT                        Video file, AviSynth file (windows only)");
 			Console.WriteLine();
 			Console.WriteLine("NEMU home page: <https://x265.github.io/>");
 			Console.WriteLine("Report bugs to: <https://github.com/Anime4000/IFME/issues>");
-			Console.WriteLine("IFME fb page  : <https://fb.com/internetfriendlymediaencoder/>");
+			Console.WriteLine("Facebook page : <https://fb.com/internetfriendlymediaencoder/>");
 		}
 
 		static void Master()
@@ -111,7 +132,7 @@ namespace nemu
 
 		static void LoadConfig()
 		{
-			IniData Data = new FileIniDataParser().ReadFile("config.ini", Encoding.UTF8);
+			IniData Data = new FileIniDataParser().ReadFile(Config, Encoding.UTF8);
 
 			Id = int.Parse(Data["config"]["id"]);
 			WatchFolder = Data["config"]["folder"];
@@ -120,7 +141,15 @@ namespace nemu
 			BinHEVC08 = Data["binary"]["08bit"];
 			BinHEVC10 = Data["binary"]["10bit"];
 			BinHEVC12 = Data["binary"]["12bit"];
+
+			if (!string.IsNullOrEmpty(Folder))
+				WatchFolder = Folder;
 		}
+
+		static bool IsMaster { get; set; }
+
+		static string Config { get; set; } = "config.ini";
+		static string Folder { get; set; } = null;
 
 		static int Id { get; set; }
 		static string WatchFolder { get; set; }
