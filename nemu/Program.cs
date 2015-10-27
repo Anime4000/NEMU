de0e2c979219adabdb5358c1d4042ad61c0dd031
Anime4000 <ilham92_sakura@yahoo.com>
2015-10-27 13:36:16 +0800

    better nemu

--- nemu/Program.cs
+++ nemu/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.IO;
 
 using IniParser;
 using IniParser.Model;
+using MediaInfoDotNet;
 
 namespace nemu
 {
@@ -18,79 +20,50 @@ namespace nemu
 
 			if (args.Length > 0)
 			{
-				for (int i = 0; i < args.Length; i++)
+				if (string.Equals(args[0], "master", IC))
 				{
-					if (args[i][0] == '-')
+					for (int i = 1; i < args.Length; i++)
 					{
-						if (args[i].Length > 1)
+						if (args[i][0] == '-')
 						{
-							if (args[i][1] == '-')
+							if (args[i].Length > 0)
 							{
-								if (args[i].Contains("help"))
+								if (args[i][1] == 'w')
 								{
-									Help();
+									DirWatch = args[++i];
 								}
 
-								if (args[i].Contains("master"))
+								if (args[i][1] == 'i')
 								{
-									IsMaster = true;
+									Input = args[++i];
 								}
 
-								if (args[i].Contains("slave"))
+								if (args[i][1] == 'e')
 								{
-									IsMaster = false;
+									Encoder = args[++i];
 								}
 
-								if (args[i].Contains("config"))
+								if (args[i][1] == 'b')
 								{
-									Config = args[++i];
-								}
-
-								if (args[i].Contains("dir"))
-								{
-									Folder = args[++i];
-								}
-							}
-
-							for (int j = 1; j < args[i].Length; j++)
-							{
-								if (args[i][j] == 'h')
-								{
-									Help();
-								}
-
-								if (args[i][j] == 'v')
-								{
-									Console.WriteLine("Hi");
-									return;
-								}
-
-								if (args[i][j] == 'm')
-								{
-									IsMaster = true;
-								}
-
-								if (args[i][j] == 's')
-								{
-									IsMaster = false;
-								}
-
-								if (args[i][j] == 'c')
-								{
-									Config = args[++i];
-								}
-
-								if (args[i][j] == 'd')
-								{
-									Folder = args[++i];
+									BitDepth = Convert.ToInt32(args[i]);
 								}
 							}
 						}
+
+						if (args[i][0] == '(')
+						{
+							while (args[i][0] != ')')
+							{
+								EncArgs += $"\"{args[i++]}\" ";
+							}
+						}
 					}
-					else
-					{
-						//
-					}
+
+					Master();
+				}
+				else
+				{
+					
 				}
 			}
 			else
@@ -102,27 +75,40 @@ namespace nemu
 		static void Help()
 		{
 			Console.WriteLine();
-			Console.WriteLine("Usage: nemu [OPTION...] INPUT");
+			Console.WriteLine("Usage: nemu [master|slave] -w [folder] -i [input] -e [x264|x265] -b [8|10|12] ( [encoder args] )");
 			Console.WriteLine();
-			Console.WriteLine("Mandatory arguments to long options are mandatory for short options too.");
+			Console.WriteLine("Main option:");
+			Console.WriteLine("  master                       Start NEMU as master server (implies slave)");
+			Console.WriteLine("  slave                        Start NEMU as salve server");
+			Console.WriteLine("  -w                           Folder path that slave server can see");
 			Console.WriteLine();
-			Console.WriteLine("Option:");
-			Console.WriteLine("  -h, --help                   show this help and exit");
-			Console.WriteLine("  -v                           show version and exit");
-			Console.WriteLine("  -m, --master                 start NEMU as master server");
-			Console.WriteLine("  -s, --slave                  start NEMU as slave server (impiles -m)");
-			Console.WriteLine("  -c, --config CONFIG.INI      (optional) override default path config.ini");
-			Console.WriteLine("  -d, --dir FOLDER             (optional) override default from config.ini");
-			Console.WriteLine("  INPUT                        Video file, AviSynth file (windows only)");
+			Console.WriteLine("Master option:");
+			Console.WriteLine("  -i                           Input video or AviSynth");
+			Console.WriteLine("  -e                           Use x264 or x265 encoder");
+			Console.WriteLine("  -b                           Set a Bit-Depth 8, 10 or 12");
+			Console.WriteLine("  encoder args                 All possible x264 or x265 argument");
+			Console.WriteLine();
+			Console.WriteLine("Example:");
+			Console.WriteLine($"  nemu master -w {(OS.IsLinux ? "/mnt/nas" : "d:\\share")} -i {(OS.IsLinux ? "~/" : "d:\\")}video.mkv -e x265 -b 10 ( --preset veryslow --tune ssim --crf 26 )");
+			Console.WriteLine($"  nemu slave -w {(OS.IsLinux ? "/mnt/nas" : "z:\\")}");
+			Console.WriteLine();
+			Console.WriteLine("Due security reasons, NEMU is relying on Network Drive or Shared Folder to avoid attack/exploit");
 			Console.WriteLine();
 			Console.WriteLine("NEMU home page: <https://x265.github.io/>");
-			Console.WriteLine("Report bugs to: <https://github.com/Anime4000/IFME/issues>");
+			Console.WriteLine("Report bugs to: <https://github.com/Anime4000/NEMU/issues>");
 			Console.WriteLine("Facebook page : <https://fb.com/internetfriendlymediaencoder/>");
 		}
 
 		static void Master()
 		{
-			
+			if (string.Equals(Path.GetExtension(Input), ".avs", IC))
+			{
+
+			}
+			else
+			{
+
+			}
 		}
 
 		static void Slave()
@@ -132,29 +118,31 @@ namespace nemu
 
 		static void LoadConfig()
 		{
-			IniData Data = new FileIniDataParser().ReadFile(Config, Encoding.UTF8);
+			IniData data = new FileIniDataParser().ReadFile(FileConfig);
 
-			Id = int.Parse(Data["config"]["id"]);
-			WatchFolder = Data["config"]["folder"];
-			BinAvsPipe = Data["binary"]["avidec"];
-			BinFFmpeg = Data["binary"]["ffmpeg"];
-			BinHEVC08 = Data["binary"]["08bit"];
-			BinHEVC10 = Data["binary"]["10bit"];
-			BinHEVC12 = Data["binary"]["12bit"];
-
-			if (!string.IsNullOrEmpty(Folder))
-				WatchFolder = Folder;
+			BinFFmpeg = data["binary"]["ffmpeg"];
+			BinAvs2pipe = data["binary"]["avs2pipe"];
+			BinAVC08 = data["x264"]["08"];
+			BinAVC10 = data["x264"]["10"];
+			BinHEVC08 = data["x265"]["08"];
+			BinHEVC10 = data["x265"]["10"];
+			BinHEVC12 = data["x265"]["12"];
 		}
 
-		static bool IsMaster { get; set; }
+		static readonly StringComparison IC = StringComparison.InvariantCultureIgnoreCase;
+		static readonly string InstallDir = OS.IsLinux ? Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]) : AppDomain.CurrentDomain.BaseDirectory;
 
-		static string Config { get; set; } = "config.ini";
-		static string Folder { get; set; } = null;
+		static string FileConfig { get; set; } = Path.Combine(InstallDir, "nemu.ini");
+		static string DirWatch { get; set; }
+		static string Input { get; set; }
+		static string Encoder { get; set; }
+		static int BitDepth { get; set; }
+		static string EncArgs { get; set; }
 
-		static int Id { get; set; }
-		static string WatchFolder { get; set; }
-		static string BinAvsPipe { get; set; }
 		static string BinFFmpeg { get; set; }
+		static string BinAvs2pipe { get; set; }
+		static string BinAVC08 { get; set; }
+		static string BinAVC10 { get; set; }
 		static string BinHEVC08 { get; set; }
 		static string BinHEVC10 { get; set; }
 		static string BinHEVC12 { get; set; }
